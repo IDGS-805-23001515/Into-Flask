036c47b8c6d5a633fa585edce6ded29604502a59
Victor Santiago <victorsantiagoaraujo8@gmail.com>
2026-01-27 22:42:01 -0600

    Mensajes Flask

---
+++ forms.py
@@ -0,0 +1,21 @@
+from wtforms import Form
+from wtforms import IntegerField, StringField, PasswordField
+from wtforms import EmailField 
+from wtforms import validators
+
+class USerForm(Form):
+    matricula=IntegerField("Matricula",[
+    validators.DataRequired(message="El campo es requerido"),
+    validators.NumberRange(min=100, max=1000, message="Ingrese el valor valido")
+    ])
+    nombre=StringField("Nombre",[
+    validators.DataRequired(message="El campo es requerido"),
+    validators.length(min=4, max=10, message="Ingrese un nombre valido")
+    ])
+    apaterno=StringField("Apaterno")
+    validators.DataRequired(message="El campo es requerido")
+    
+    amaterno=StringField("Amaterno")
+    validators.DataRequired(message="El campo es requerido")
+    correo=EmailField('Correo')
+    validators.DataRequired(message="El campo es requerido")
